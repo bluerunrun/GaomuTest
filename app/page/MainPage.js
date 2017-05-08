0e93a8a8366a6d9d688b24bb1e8cc4ab6e58de59
guopu Lan <bluerunrun.freeman@gmail.com>
2017-05-09 00:48:22 +0800

    creat branch d0509

--- app/page/MainPage.js
+++ app/page/MainPage.js
@@ -10,37 +10,52 @@ import {
   WebView,
   Alert,
   Button,
+  TextInput,
 
 } from'react-native';
-const html = require('../src/react.html');
+import Dimensions from 'Dimensions';
+const defaultUrl = require('../src/rn-react.html');
 
 
 export default class MainPage extends Component {
 
+    webview: WebView;
+
     constructor(props) {
         super(props);
-    }
-    webview: null;
-
-    handleMessage(event){
-      console.log('onMessage->event.nativeEvent.data:');
-      console.log(event.nativeEvent.data);
-      Alert.alert(
-        '警告提示',
-        event.nativeEvent.data,
-        [
-          {
-              text: 'OK',
-              onPress: () => console.log("Press OK!"),
-          },
-        ]
-      );
+        this.state = {
+            inputValue: "",
+            html: defaultUrl
+        };
     }
 
-    postMessage = () => {
+    handleTextChange = inputValue => {
+        this.setState({ inputValue });
+    };
+
+    navToAnotherUrl =  html => {
+        this.setState({html: require('../src/react.html')});
+    };
+
+    webListener(event){
+        console.log('onMessage->event.nativeEvent.data:');
+        console.log(event.nativeEvent.data);
+        Alert.alert(
+            'Get Data From Web',
+            event.nativeEvent.data,
+            [
+                {
+                    text: 'OK',
+                    onPress: () => console.log("Press OK!"),
+                },
+            ]
+        );
+    }
+
+    rnSender = () => {
         if (this.webview) {
-            // alert(this.webview);
-            this.webview.postMessage('"Hello" from React Native!');
+            var msg = this.state.inputValue;
+            this.webview.postMessage(msg===""?"text":msg);
         }
     }
 
@@ -48,26 +63,66 @@ export default class MainPage extends Component {
         return (
             <View style={{flex:1}}>
                 <WebView
-                    style={styles.webview_style}
+                    style={styles.web_side_style}
                     ref={webview => { this.webview = webview; }}
-                    source={html}
+                    source={this.state.html}
                     startInLoadingState={true}
                     domStorageEnabled={true}
                     javaScriptEnabled={true}
-                    onMessage={this.handleMessage}
+                    onMessage={this.webListener}
                 >
                 </WebView>
-                <View >
-                    <Button title="Send Message to Web View"  onPress={this.postMessage} />
+                <View style={styles.space_style}></View>
+                <View style={styles.rn_side_style}>
+                    <Text style={styles.text_style}>React Native Side: </Text>
+                    <TextInput style={styles.input_style}
+                               placeholder=' 请输入'
+                               returnKeyType={'done'}
+                               underlineColorAndroid='transparent'
+                               value={this.state.inputValue}
+                               onChangeText={this.handleTextChange.bind(this)}
+                    />
+                    <Button title="Send Message to Web View"  onPress={this.rnSender} />
+
+                    <Button title="Nav Web View To Another Page"  onPress={this.navToAnotherUrl.bind(this)} />
                 </View>
             </View>
         );
     }
 }
 
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const MARGIN = 80;
+
 const styles = StyleSheet.create({
-  webview_style:{
-    backgroundColor:'#00ff00',
-  }
+    web_side_style:{
+        backgroundColor: "white",
+        flex: 1,
+    },
+    rn_side_style:{
+        backgroundColor: "white",
+        flex: 1,
+    },
+    space_style:{
+        backgroundColor: "black",
+        height: 2,
+    },
+    text_style: {
+        marginTop: 22,
+        marginLeft: 10,
+        fontSize: 24,
+    },
+    input_style: {
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        borderColor:'black',
+        borderWidth: 1,
+        height:30,
+        borderRadius: 5,
+        marginHorizontal: 20,
+        paddingLeft:12,
+        color: 'black',
+    },
+
 });
 
